00d65922c65bd5a95bfb70bdc02d635fad58c47f
Marco <marcogt@me.com>
2019-06-10 10:44:41 +0200

    Initial Commit

---
+++ GestoreDispensa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestoreDispensa
+//
+//  Created by Marco on 10.06.19.
+//  Copyright © 2019 Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
